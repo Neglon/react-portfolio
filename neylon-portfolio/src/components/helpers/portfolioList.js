989bd509cb32675343a6bd034f54f98e294d4936
Neglon <thomasfneylon@gmail.com>
2024-04-24 15:00:47 -0400

    portfolio update

--- neylon-portfolio/src/components/helpers/portfolioList.js
+++ neylon-portfolio/src/components/helpers/portfolioList.js
@@ -1,23 +1,45 @@
-import Project1 from '../images/bullseye.PNG';
+import Project1 from '../images/css.PNG';
 import Project2 from '../images/movieMingle.PNG';
 import Project3 from '../images/quotely.PNG';
 import Project4 from '../images/weatherDashboard.PNG';
+import Project5 from '../images/jate.PNG';
+import Project6 from '../images/bullseye.PNG';
 
 export const PortfolioList = [
     {
-        name: "BullsEye",
+        name: "CSS Cheatsheet",
         image: Project1,
+        githubUrl: "https://github.com/Neglon/minitwo",
+        deployedUrl: "https://neglon.github.io/minitwo",
     },
     {
         name: "Movie Mingle",
         image: Project2,
+        githubUrl: "https://github.com/Neglon/movie-mingle",
+        deployedUrl: "https://neglon.github.io/movie-mingle/",
     },
     {
         name: "Quotely",
         image: Project3,
+        githubUrl: "https://github.com/dileknrdn/quotely",
+        deployedUrl: "https://quotely-22947f0a031d.herokuapp.com/",
     },
     {
         name: "Weather Dashboard",
         image: Project4,
+        githubUrl: "https://github.com/Neglon/weather-dashboard",
+        deployedUrl: "https://neglon.github.io/weather-dashboard/",
+    },
+    {
+        name: "JATE",
+        image: Project5,
+        githubUrl: "https://github.com/Neglon/PWA-Text-Editor",
+        deployedUrl: "https://pwa-text-editor-ic0s.onrender.com/",
+    },
+    {
+        name: "Bullseye",
+        image: Project6,
+        githubUrl: "https://github.com/Neglon/BullsEye",
+        deployedUrl: "",
     }
 ]
--- neylon-portfolio/src/components/portfolioItem.jsx
+++ neylon-portfolio/src/components/portfolioItem.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 
-function PortfolioItem({image, name}) {
+function PortfolioItem({image, name, githubUrl, deployedUrl}) {
     return (
         <div className="portfolioItem">
             <div style={{ backgroundImage: `url(${image})` }} className="bgImage" />
             <h1> {name} </h1>
-            
+            <div className="portfolioButtons">
+                <a href={githubUrl} target="_blank" rel="noopener noreferrer" className="portfolioButton github">Github</a>
+                <a href={deployedUrl} target="_blank" rel="noopener noreferrer" className="portfolioButton deployed">Live</a>
+            </div>
         </div>
     );
 }
